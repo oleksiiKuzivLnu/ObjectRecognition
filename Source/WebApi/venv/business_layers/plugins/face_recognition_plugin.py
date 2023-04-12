1141e8c4dbfbc0cba196554639f0aa53d047d3e5
dmytro-alianakh-lnu <130457775+dmytro-alianakh-lnu@users.noreply.github.com>
2023-04-12 04:51:24 -0700

    Add Neural Network to detect faces on image
    Signed-off-by: dmytro-alianakh-lnu <130457775+dmytro-alianakh-lnu@users.noreply.github.com>

--- Source/WebApi/venv/business_layers/plugins/face_recognition_plugin.py
+++ Source/WebApi/venv/business_layers/plugins/face_recognition_plugin.py
@@ -1,9 +1,28 @@
 from typing import List
 from .image_processor_plugin import ImageProcessorPlugin
 from ...domains.models.image_artifact import ImageArtifact
+import cv2
+import numpy as np
+from ultralytics import YOLO
+
+model = YOLO('./best.pt')
+class_name_dict = {0: 'face'}
+threshold = 0.0001
 
 
 class FaceRecognitionPlugin(ImageProcessorPlugin):
     def process(self, input: List[ImageArtifact]) -> List[ImageArtifact]:
-        # face recognition plugin logic
-        pass
+        img_np = np.frombuffer(input[0]._data, dtype=np.uint8)
+        photo = cv2.imdecode(img_np, cv2.IMREAD_COLOR)
+        img = photo
+        results= model(img)[0].boxes.data.tolist()
+        for result in results:
+            x1, y1, x2, y2, score, class_id = result
+            if score > threshold:
+                cv2.rectangle(img, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 4)
+                cv2.putText(img, class_name_dict[int(class_id)].upper(), (int(x1), int(y1 - 10)),
+                cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3, cv2.LINE_AA)
+
+        _, img_bytearray = cv2.imencode('.jpg', img)
+        res = ImageArtifact(img_bytearray)
+        return [res]
