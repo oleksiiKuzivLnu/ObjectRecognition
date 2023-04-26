73b2c2cfebd2509f43638336b0c21731bb83e8a3
dmytro-alianakh-lnu <130457775+dmytro-alianakh-lnu@users.noreply.github.com>
2023-04-26 04:38:49 -0700

    Add converting instance of class to object
    Signed-off-by: dmytro-alianakh-lnu <130457775+dmytro-alianakh-lnu@users.noreply.github.com>

--- Source/WebApi/venv/models/responses/input_media_process_response.py
+++ Source/WebApi/venv/models/responses/input_media_process_response.py
@@ -3,3 +3,7 @@
 class InputMediaProcessResponse:
     def __init__(self, images):
         self.images = images
+    def to_dict(self):
+        return {
+            "images": self.images
+        }
