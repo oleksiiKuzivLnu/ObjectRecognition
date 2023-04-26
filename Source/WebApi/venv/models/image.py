4df9dba1367e4f20b2232ed38c10b4bf2e2aa01e
dmytro-alianakh-lnu <130457775+dmytro-alianakh-lnu@users.noreply.github.com>
2023-04-26 04:38:18 -0700

    Add converting instance of class to object
    Signed-off-by: dmytro-alianakh-lnu <130457775+dmytro-alianakh-lnu@users.noreply.github.com>

--- Source/WebApi/venv/models/image.py
+++ Source/WebApi/venv/models/image.py
@@ -1,5 +1,12 @@
-
+import json
+import base64
 
 class Image:
     def __init__(self, data):
         self.data = data
+    def to_dict(self):
+        img_base64 = base64.b64encode(self.data)
+        img_base64_string = img_base64.decode("utf-8")
+        return {
+            "return": json.dumps({"image": img_base64_string})
+        }
