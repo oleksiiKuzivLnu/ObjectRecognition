45d24b58ea7f6fcb526db0888f0c431f0aae64b3
dmytro-alianakh-lnu <130457775+dmytro-alianakh-lnu@users.noreply.github.com>
2023-04-26 04:49:21 -0700

    Update base64 format
    Signed-off-by: dmytro-alianakh-lnu <130457775+dmytro-alianakh-lnu@users.noreply.github.com>

--- Source/WebApi/venv/models/image.py
+++ Source/WebApi/venv/models/image.py
@@ -8,5 +8,5 @@ class Image:
         img_base64 = base64.b64encode(self.data)
         img_base64_string = img_base64.decode("utf-8")
         return {
-            "return": json.dumps({"image": img_base64_string})
+            "return": json.dumps({"image": "data:image/jpeg;base64," + img_base64_string})
         }
