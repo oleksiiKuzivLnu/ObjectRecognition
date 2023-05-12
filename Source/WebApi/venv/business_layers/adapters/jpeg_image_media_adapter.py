22473c590b69e88fcf4f90a7edf621b6c5e78e8f
YuliaYur <40237840+YuliaYur@users.noreply.github.com>
2023-05-12 22:13:52 +0300

    Move bytes -> img converting logic to JpegImageMediaAdapter.toImageArtifacts. Add JpegImageMediaAdapter.fromImageArtifacts

--- Source/WebApi/venv/business_layers/adapters/jpeg_image_media_adapter.py
+++ Source/WebApi/venv/business_layers/adapters/jpeg_image_media_adapter.py
@@ -1,4 +1,8 @@
-from typing import List, Dict
+from typing import List
+
+import numpy as np
+import cv2
+
 from .abstract_media_adapter import AbstractMediaAdapter
 from ...domains.models.media_file import MediaFile
 from ...domains.models.image_artifact import ImageArtifact
@@ -7,7 +11,18 @@ from ...domains.models.image_artifact import ImageArtifact
 class JpegImageMediaAdapter(AbstractMediaAdapter):
     def toImageArtifacts(self, mediaFile: MediaFile) -> List[ImageArtifact]:
         # TODO: Review as it is temporary
-        buf = bytearray(mediaFile._data)
+        buf = bytearray(mediaFile.data)
         bytes_buf = bytes(buf)
 
-        return [ImageArtifact(bytes_buf, {})]
+        img_np = np.frombuffer(bytes_buf, dtype=np.uint8)
+        img = cv2.imdecode(img_np, cv2.IMREAD_COLOR)
+
+        return [ImageArtifact(img, {})]
+
+    def fromImageArtifacts(self, imageArtifacts: List[ImageArtifact]) -> bytes:
+        assert len(imageArtifacts) == 1
+
+        img = imageArtifacts[0].data
+        _, img_bytearray = cv2.imencode('.jpg', img)
+
+        return img_bytearray
