e03b43dd7588f7f24fe962eec9cbc32e6832ce34
YuliaYur <40237840+YuliaYur@users.noreply.github.com>
2023-05-12 22:10:39 +0300

    Make Image be initialized with MediaFile instead of bytes

--- Source/WebApi/venv/models/image.py
+++ Source/WebApi/venv/models/image.py
@@ -1,10 +1,13 @@
-import json
 import base64
 
+from ..domains.models.media_file import MediaFile
+
+
 class Image:
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, file: MediaFile):
+        self._file = file
+
     def to_dict(self):
-        img_base64 = base64.b64encode(self.data)
+        img_base64 = base64.b64encode(self._file.data)
         img_base64_string = img_base64.decode("utf-8")
-        return "data:image/jpeg;base64," + img_base64_string
+        return f"data:{self._file.mediaType};base64," + img_base64_string
