237e7eb52b8c42d3a448649c57389fac6a727c53
YuliaYur <40237840+YuliaYur@users.noreply.github.com>
2023-05-12 22:11:48 +0300

    Add properties to ImageArtifact and MediaFile to not access protected fields

--- Source/WebApi/venv/domains/models/media_file.py
+++ Source/WebApi/venv/domains/models/media_file.py
@@ -2,3 +2,11 @@ class MediaFile:
     def __init__(self, data: bytes, mediaType: str):
         self._data = data
         self._mediaType = mediaType
+
+    @property
+    def data(self) -> bytes:
+        return self._data
+
+    @property
+    def mediaType(self) -> str:
+        return self._mediaType
