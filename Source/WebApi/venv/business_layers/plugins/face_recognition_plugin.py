7bd39259a62dd6f1dbd5345ae6c56013e0541255
YuliaYur <40237840+YuliaYur@users.noreply.github.com>
2023-05-12 22:15:51 +0300

    Update FaceRecognitionPlugin accordingly to ImageArtifact.data type update

--- Source/WebApi/venv/business_layers/plugins/face_recognition_plugin.py
+++ Source/WebApi/venv/business_layers/plugins/face_recognition_plugin.py
@@ -1,10 +1,12 @@
 from typing import List
+import os
+
+import cv2
+from ultralytics import YOLO
+
 from .image_processor_plugin import ImageProcessorPlugin
 from ...domains.models.image_artifact import ImageArtifact
-import cv2
-import numpy as np
-from ultralytics import YOLO
-import os
+
 
 model = YOLO(os.path.abspath(r"./venv/business_layers/plugins/best.pt"))
 class_name_dict = {0: 'face'}
@@ -13,15 +15,16 @@ threshold = 0.0001
 
 class FaceRecognitionPlugin(ImageProcessorPlugin):
     def process(self, input: List[ImageArtifact]) -> List[ImageArtifact]:
-        img_np = np.frombuffer(input[0]._data, dtype=np.uint8)
-        photo = cv2.imdecode(img_np, cv2.IMREAD_COLOR)
-        img = photo
-        results= model(img)[0].boxes.data.tolist()
-        for result in results:
-            x1, y1, x2, y2, score, class_id = result
-            if score > threshold:
-                cv2.rectangle(img, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 4)
+        output = []
 
-        _, img_bytearray = cv2.imencode('.jpg', img)
-        res = ImageArtifact(img_bytearray, {})
-        return [res]
+        for image_artifact in input:
+            img = image_artifact.data
+            results = model(img)[0].boxes.data.tolist()
+            for result in results:
+                x1, y1, x2, y2, score, class_id = result
+                if score > threshold:
+                    cv2.rectangle(img, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 4)
+
+            output.append(ImageArtifact(img, image_artifact.metadata))
+
+        return output
